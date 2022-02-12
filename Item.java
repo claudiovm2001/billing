a3c7460def4168849a32b183e9d94e9d34205909
Claudio Vitor <claudiovitor2001@gmail.com>
2022-02-12 13:17:09 -0300

    define initial structure

---
+++ Item.java
@@ -0,0 +1,13 @@
+public class Item {
+    private int code;
+    private String name;
+    private float price;
+    private int quantity;
+
+    public void Item(int code, String name, float price, int quantity){
+        this.code = code;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
---
+++ Program.java
@@ -0,0 +1,18 @@
+public class Program {
+
+    public Item insert(){
+
+        Item item = new Item();
+        return item;
+    }
+
+    public void Review(){
+
+    }
+
+    public Boolean start(){
+        
+        System.out.println("||Sistema de faturamento automático||");
+        return true;
+    }
+}

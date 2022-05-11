3eb5f625ffe82350eb959e2d65df0e87327fe727
claud <claud@192.168.1.11>
2022-05-11 12:38:45 -0300

    implemented initial employee CRUD

---
+++ SysPVC/src/AdminPanel_1.java
@@ -0,0 +1,133 @@
+import java.awt.EventQueue;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JList;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class AdminPanel_1 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AdminPanel_1 window = new AdminPanel_1();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AdminPanel_1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setMinimumSize(new Dimension(640, 480));
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		String[] employees;
+		
+		int size = 0;
+		
+		Connection c = null;
+		
+		try {
+			c = DriverManager.getConnection(
+					"jdbc:postgresql://localhost:5432/postgres",
+			        "postgres", "30042001");
+			
+			//c.setAutoCommit(false);
+			
+			//CONSULTAR
+			Statement stmt = null;
+			stmt = c.createStatement();
+			
+			ResultSet rs = stmt.executeQuery( "SELECT * FROM \"EMPLOYEES\";" );
+			while ( rs.next() ) {
+				++size;
+			}
+			c.close();
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		employees = new String[size];
+		
+		int cont = 0;
+		
+		c = null;
+		
+		try {
+			c = DriverManager.getConnection(
+					"jdbc:postgresql://localhost:5432/postgres",
+			        "postgres", "30042001");
+			
+			
+			//CONSULTAR
+			Statement stmt = null;
+			stmt = c.createStatement();
+			
+			ResultSet rs = stmt.executeQuery( "SELECT * FROM \"EMPLOYEES\";" );
+			while ( rs.next() ) {
+				employees[cont] = rs.getString("username");
+				++cont;
+			}
+			
+			c.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JLabel lblNewLabel_1 = new JLabel("Cadastros");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_1.setBounds(245, 38, 103, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(215, 117, 160, 313);
+		frame.getContentPane().add(panel);
+		
+		JList list = new JList(employees);
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addComponent(list, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 160, Short.MAX_VALUE)
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addComponent(list, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 324, Short.MAX_VALUE)
+		);
+		panel.setLayout(gl_panel);
+	}
+}
